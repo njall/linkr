af06192707ab89b11cc8dedac654b054eb520f11
Niall Beard <niallbeard@gmail.com>
2020-12-05 20:23:41 +0000

    Associated newly created links with current_user

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -25,7 +25,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.new(link_params)
 
     respond_to do |format|
       if @link.save
