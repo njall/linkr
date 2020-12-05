bd0f978f270b3ca98f0a7141fbade35fb8f7becc
Niall Beard <niallbeard@gmail.com>
2020-12-05 18:28:12 +0000

    Added spec tests for Link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.create!(email: "bob@example.com", password: "horses", first_name: "Bob")
+  end
+
+
+  context 'valid slug and url' do
+    it 'creates a link' do
+      expect { @user.links.create(slug: "blah", url: "http://google.com") }.to change{ Link.count }.by(1)
+    end
+  end
+
+  context 'blank slug or url' do
+    it 'should give errors' do
+      expect { @user.links.create!(slug: '', url: '') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+
 end
