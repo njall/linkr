ba87f84089365d981363405ffa87f3d170f6b8cf
Niall Beard <niallbeard@gmail.com>
2020-12-05 18:29:04 +0000

    Add Link validation and define user-link association'

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,13 @@
 class Link < ApplicationRecord
   belongs_to :user
+
+  validates :url, :slug, presence: true, allow_blank: false
+
+  after_create do |record|
+    record.update(counter: 0)
+  end
+
+  def increment_count!
+    update(counter: counter + 1)
+  end
 end
