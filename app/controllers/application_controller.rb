17353c410a541c02b109e014249652fed9bda23d
Niall Beard <niallbeard@gmail.com>
2020-12-05 19:54:52 +0000

    Authenticate user before accessing link pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path, :notice => 'You must be logged in to see this page'
+    end
+  end
+
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: %w[ redirect ]
 
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
   # GET /links
   # GET /links.json
   def index
