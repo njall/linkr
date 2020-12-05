c4870512fba29e53d59660e8ead2b64acb6fa166
Niall Beard <niallbeard@gmail.com>
2020-12-05 18:30:07 +0000

    Remove extra email definition from devise migration

--- db/migrate/20201205181151_devise_create_users.rb
+++ db/migrate/20201205181151_devise_create_users.rb
@@ -33,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
       t.string :first_name
-      t.string :email
 
       t.timestamps null: false
     end
