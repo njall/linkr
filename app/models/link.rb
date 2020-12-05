d976da2cc75f668be652bc2fc23fa1e3c4dd3dbb
Niall Beard <niallbeard@gmail.com>
2020-12-05 21:06:13 +0000

    URL and slug validation and tests

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,42 @@
 class Link < ApplicationRecord
   belongs_to :user
 
-  validates :url, :slug, presence: true, allow_blank: false
+  validates :slug, uniqueness: true, presence: true, allow_blank: false
+  validates :url, presence: true, allow_blank: false
+
+  validate :url_matches_pattern,
+           :url_has_no_restricted_words,
+           :slug_matches_pattern,
+
+
+
+  def url_matches_pattern
+      errors.add(:url, "must begin with http://") unless url =~ /https?:\/\/[\w|.]+$/
+  end
+
+  def url_has_no_restricted_words
+    %w[links users].each do |word|
+      errors.add(:url, "cannot begin with #{word}") if url =~ /$#{word}\/\/[\w|.]+$/
+    end
+  end
+
+  def slug_matches_pattern
+    errors.add(:slug, "must only contain numbers and letters") unless slug =~ /^[\w]+$/
+  end
+
+
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date.present? && expiration_date < Date.today
+      errors.add(:expiration_date, "can't be in the past")
+    end
+  end
+
+  def discount_cannot_be_greater_than_total_value
+    if discount > total_value
+      errors.add(:discount, "can't be greater than total value")
+    end
+  end
+
 
   after_create do |record|
     record.update(counter: 0)
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -24,6 +24,41 @@ RSpec.describe Link, type: :model do
     end
   end
 
+  context 'url or slug with spaces in' do
+    it 'should give errors' do
+      expect { @user.links.create!(slug: 'this is not ideal for a url', url: 'http://google.com') }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { @user.links.create!(slug: 'slug', url: 'http://google.com or something') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'incorrect url format' do
+    it 'should give errors' do
+      expect { @user.links.create!(slug: 'slug', url: 'htpak31') }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { @user.links.create!(slug: 'slug', url: 'http//:nope') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'slug already exists' do
+    it 'should give errors' do
+      @user.links.create!(slug: 'slug', url: 'http://google.com')
+      expect { @user.links.create!(slug: 'slug', url: 'http://twoogle.com') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'hyphens and underscores are okay for slugs' do
+    it 'should give errors' do
+      expect { @user.links.create!(slug: 'slug-with-hyphen_and_underscore', url: 'http://twoogle.com') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'slug should not start with links or users' do
+    it 'should give errors' do
+      expect { @user.links.create!(slug: 'links/3', url: 'http://twoogle.com') }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { @user.links.create!(slug: 'users/new', url: 'http://twoogle.com') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+
   context '#increment_counter!' do
     before do
       @link = @user.links.create(slug: "blah", url: "http://google.com")
