ae79324b89761298bf3fe7ef0720ef60722e3620
Niall Beard <niallbeard@gmail.com>
2020-12-07 08:41:17 +0000

    Remove pasted example code

--- app/models/link.rb
+++ app/models/link.rb
@@ -24,20 +24,6 @@ class Link < ApplicationRecord
     errors.add(:slug, "must only contain numbers, letters, hyphens and/or underscores") unless slug =~ /^[\w|\-|\_]+$/
   end
 
-
-  def expiration_date_cannot_be_in_the_past
-    if expiration_date.present? && expiration_date < Date.today
-      errors.add(:expiration_date, "can't be in the past")
-    end
-  end
-
-  def discount_cannot_be_greater_than_total_value
-    if discount > total_value
-      errors.add(:discount, "can't be greater than total value")
-    end
-  end
-
-
   after_create do |record|
     record.update(counter: 0)
   end
